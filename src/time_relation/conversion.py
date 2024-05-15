bd6d81e281cb1a27fdffe7f410ec0896d2f51326
kobatakeyann <suu_hppa@outlool.jp>
2024-05-15 15:48:18 +0900

    import the specific class or method.

--- src/time_relation/conversion.py
+++ src/time_relation/conversion.py
@@ -1,4 +1,4 @@
-import datetime
+from datetime import datetime, timedelta
 
 def get_factors_of_datetime(target_datetime) -> tuple:
     year = str(target_datetime.year).zfill(4)
@@ -8,13 +8,13 @@ def get_factors_of_datetime(target_datetime) -> tuple:
     minute = str(target_datetime.minute).zfill(2)
     return year,month,day,hour,minute
 
-def jst_to_utc(jst_datetime) -> datetime.datetime:
-    time_difference = datetime.timedelta(hours=9)
+def jst_to_utc(jst_datetime) -> datetime:
+    time_difference = timedelta(hours=9)
     utc_datetime = jst_datetime - time_difference
     return utc_datetime 
 
-def utc_to_jst(utc_datetime) -> datetime.datetime:
-    time_difference = datetime.timedelta(hours=9)
+def utc_to_jst(utc_datetime) -> datetime:
+    time_difference = timedelta(hours=9)
     jst_datetime = utc_datetime + time_difference
     return jst_datetime 
 
