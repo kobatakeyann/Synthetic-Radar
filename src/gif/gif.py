532f6542fb8995d8c4bdb7eba9cd38149b9492ca
kobatakeyann <suu_hppa@outlool.jp>
2024-05-15 16:15:07 +0900

    modify the import part and variable names.

--- src/gif/gif.py
+++ src/gif/gif.py
@@ -1,14 +1,14 @@
-import glob
 import os
+from glob import glob
 from PIL import Image
 
-def convert_jpg_to_gif(parent_path,dir_out,gif_name):
+def convert_jpg_to_gif(parent_path,gif_out,gif_name):
     img_array = []
-    for filename in sorted(glob.glob(f"{parent_path}/*.jpg")):
-        img = Image.open(filename)
+    for img_filename in sorted(glob(f"{parent_path}/*.jpg")):
+        img = Image.open(img_filename)
         img.thumbnail((1200,800))
         img_array.append(img)
 
-        out_path = os.path.join(dir_out,gif_name)
+        out_path = os.path.join(gif_out,gif_name)
 
     img_array[0].save(out_path ,save_all=True, append_images=img_array[1:], optimize=True, duration=90, loop=0)
