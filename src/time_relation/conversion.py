7c7be798af046dad0867ed7674c099df57f0ea0b
kobatakeyann <suu_hppa@outlool.jp>
2024-05-22 15:20:35 +0900

    modify the class name, separate some functions from class

--- src/time_relation/conversion.py
+++ src/time_relation/conversion.py
@@ -1,7 +1,6 @@
 from datetime import datetime, timedelta
 
-
-class Date:
+class PaddingDate:
     def __init__(self,target_dt):
         self.target_dt = target_dt
         self.year = str(target_dt.year).zfill(4)
@@ -10,13 +9,13 @@ class Date:
         self.hour = str(target_dt.hour).zfill(2)
         self.minute = str(target_dt.minute).zfill(2)
 
-    def jst_to_utc(self) -> datetime:
-        time_difference = timedelta(hours=9)
-        self.utc_dt = self.target_dt - time_difference
-        return self.utc_dt 
+def jst_to_utc(datetime) -> datetime:
+    time_difference = timedelta(hours=9)
+    utc_dt = datetime - time_difference
+    return utc_dt
 
-    def utc_to_jst(self) -> datetime:
-        time_difference = timedelta(hours=9)
-        self.jst_dt = self.target_dt + time_difference
-        return self.jst_dt 
+def utc_to_jst(datetime) -> datetime:
+    time_difference = timedelta(hours=9)
+    jst_dt = datetime + time_difference
+    return jst_dt 
 
