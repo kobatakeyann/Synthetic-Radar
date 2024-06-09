86fde8bf4aa770626c9824cc65032c08ba95a901
kobatakeyann <suu_hppa@outlool.jp>
2024-06-09 23:30:28 +0900

    put an anotation.

--- src/gif/gif.py
+++ src/gif/gif.py
@@ -2,7 +2,7 @@ import os
 from glob import glob
 from PIL import Image
 
-def convert_jpg_to_gif(parent_path,saved_dir_path,gif_name):
+def convert_jpg_to_gif(parent_path:str,saved_dir_path:str,gif_name:str) -> None:
     img_array = []
     for img_filename in sorted(glob(f"{parent_path}/*.jpg")):
         img = Image.open(img_filename)
