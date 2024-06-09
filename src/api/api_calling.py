2eca4b3cb240e99b60a5e7016d69f805429b2545
kobatakeyann <suu_hppa@outlool.jp>
2024-06-10 01:47:21 +0900

    reconstruct the function of 'load_jma_gpv'.

--- src/api/api_calling.py
+++ src/api/api_calling.py
@@ -1,8 +1,11 @@
-from urllib.request import Request, urlopen, _UrlopenRet
+from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
 
+from util.tempfile_name import generate_random_string
+from util.path_complement import generate_path
 
-def get_response(url:str) -> Request:
+
+def request_to_api(url:str) -> Request:
     try:
         req = Request(url)
     except URLError as err:
@@ -10,11 +13,19 @@ def get_response(url:str) -> Request:
     else:
         return req
 
-def read_response(response:Request) -> _UrlopenRet:
+def read_response(req:Request):
     try:
-        with urlopen(response) as res:
+        with urlopen(req) as res:
             res_data = res.read()
     except HTTPError as err:
         raise HTTPError(err.url, err.code, err.reason, err.headers, err.fp)
     else:
         return res_data
+
+def write_down_res(res_data) -> str:
+    random_strings = generate_random_string(100)
+    tmpfile = generate_path(f"/tmp/tmp{random_strings}.tar")
+    with open(tmpfile, mode='wb') as f:
+        f.write(res_data)
+    output_path = tmpfile
+    return output_path
