cee1d267493c49c36f29318bdd32502eeee8a50e
kobatakeyann <suu_hppa@outlool.jp>
2024-06-15 12:57:22 +0900

    modify the api function.

--- src/api/api_calling.py
+++ src/api/api_calling.py
@@ -2,17 +2,19 @@ from urllib.request import Request, urlopen
 from urllib.error import HTTPError, URLError
 
 
-def get_response(url:str) -> Request:
+def fetch_data(url: str):
     try:
         response = Request(url)
-        return response
-    except URLError as err:
-        raise URLError(err)
-
-def read_response(response:Request):
-    try:
         with urlopen(response) as res:
             res_data = res.read()
-        return res_data
-    except HTTPError as err:
-        raise HTTPError(err.url, err.code, err.reason, err.headers, err.fp)
+            return res_data
+    except HTTPError as http_err:
+        raise HTTPError(
+            http_err.url,
+            http_err.code,
+            http_err.reason,
+            http_err.headers,
+            http_err.fp,
+        )
+    except URLError as url_err:
+        raise URLError(url_err)
