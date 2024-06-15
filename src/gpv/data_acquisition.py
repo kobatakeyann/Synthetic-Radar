79ee05564dc234467259f128d8330acab7875468
kobatakeyann <suu_hppa@outlool.jp>
2024-06-15 13:38:44 +0900

    modify the function of load_jma_gpv.

--- src/gpv/data_acquisition.py
+++ src/gpv/data_acquisition.py
@@ -5,14 +5,11 @@ from tempfile import TemporaryFile
 from nakametpy.jma import load_jmara_grib2
 
 from api.api_calling import fetch_data
-from time_relation.conversion import PaddingDate, jst_to_utc
+from time_relation.conversion import PaddingDate
 
 
-def load_jma_gpv(jst_datetime: datetime) -> MaskedArray:
+def load_jma_gpv(utc_datetime: datetime) -> MaskedArray:
     """全国合成レーダーGPVの値の配列を返す関数"""
-
-    # 0補完された日付文字列の取得
-    utc_datetime = jst_to_utc(jst_datetime)
     target_datetime = PaddingDate(utc_datetime)
     year, month, day, hour, minute = (
         target_datetime.year,
@@ -21,18 +18,14 @@ def load_jma_gpv(jst_datetime: datetime) -> MaskedArray:
         target_datetime.hour,
         target_datetime.minute,
     )
-
-    # API calling
-
     url = f"http://database.rish.kyoto-u.ac.jp/arch/jmadata/data/jma-radar/synthetic/original/{year}/{month}/{day}/Z__C_RJTD_{year}{month}{day}{hour}{minute}00_RDR_JMAGPV__grib2.tar"
     res_data = fetch_data(url)
-
-    # convert binary to array
+    # ライブラリの特性上、ファイルに書き込みを行わないと、データを取得できない
     with TemporaryFile(mode="wb") as f:
         f.write(res_data)
-        contentfile = f"Z__C_RJTD_{year}{month}{day}{hour}{minute}00_RDR_JMAGPV_Ggis1km_Prr10lv_ANAL_grib2.bin"
         gpv_array: MaskedArray = load_jmara_grib2(
-            f.name, tar_flag=True, tar_contentname=contentfile
+            file=f.name,
+            tar_flag=True,
+            tar_contentname=f"Z__C_RJTD_{year}{month}{day}{hour}{minute}00_RDR_JMAGPV_Ggis1km_Prr10lv_ANAL_grib2.bin",
         )
-
     return gpv_array
