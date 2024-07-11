7ed76d4c0a95452c8b3d62e86cfaadb1bc667360
kobatakeyann <suu_hppa@outlool.jp>
2024-07-11 21:51:16 +0900

    modify the map method

--- src/map/base_map.py
+++ src/map/base_map.py
@@ -1,6 +1,6 @@
 from axes import MapAxesMethod
 from cartopy.mpl.geoaxes import GeoAxes
-from constant import IS_DEG_MIN_FORMAT, PLOT_ELEVATION
+from constant import IS_DEG_MIN_FORMAT, PLOT_ELEVATION, PLOT_GRID_LINE
 
 
 def make_base_map(ax: GeoAxes) -> GeoAxes:
@@ -10,6 +10,8 @@ def make_base_map(ax: GeoAxes) -> GeoAxes:
     map_axes.set_ticks()
     if PLOT_ELEVATION:
         map_axes.plot_elevation_with_contour()
+    if PLOT_GRID_LINE:
+        map_axes.draw_gridlines()
     if IS_DEG_MIN_FORMAT:
         map_axes.express_in_deg_min_format()
     map_axes.narrow_down_the_plot_area()
