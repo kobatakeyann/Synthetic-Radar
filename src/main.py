b58bb2c0cd921395e3076a67fac2985da382330a
kobatakeyann <suu_hppa@outlook.jp>
2024-07-11 22:46:01 +0900

    modify the hadling of response data

--- src/main.py
+++ src/main.py
@@ -4,8 +4,8 @@ import pandas as pd
 from figure.plot import FigureFactory
 
 if __name__ == "__main__":
-    start_datetime = datetime(2021, 8, 5, 0, 0)
-    end_datetime = datetime(2021, 8, 5, 23, 50)
+    start_datetime = datetime(2023, 7, 27, 0, 0)
+    end_datetime = datetime(2023, 7, 27, 23, 50)
     jst_datetimes = pd.date_range(start_datetime, end_datetime, freq="10min")
     factory = FigureFactory(jst_datetimes)
     factory.make_continuous_figures()
