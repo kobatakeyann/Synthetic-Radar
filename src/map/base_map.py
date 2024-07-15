426b40ebd8143118952d3d9283a866d72c9abbe9
kobatakeyann <suu_hppa@outlook.jp>
2024-07-16 00:45:22 +0900

    modify import sentences

--- src/map/base_map.py
+++ src/map/base_map.py
@@ -1,5 +1,5 @@
 from cartopy.mpl.geoaxes import GeoAxes
-from constant import IS_DEG_MIN_FORMAT, PLOT_ELEVATION, PLOT_GRID_LINE
+from configuration import is_deg_min_format, is_elevation_map, is_grid_line
 
 from .axes import MapAxesMethod
 
@@ -10,11 +10,11 @@ def make_base_map(ax: GeoAxes) -> GeoAxes:
     map_axes.plot_coastline()
     map_axes.plot_pref_border()
     map_axes.set_ticks()
-    if PLOT_ELEVATION:
+    if is_elevation_map:
         map_axes.plot_elevation_with_contour()
-    if PLOT_GRID_LINE:
+    if is_grid_line:
         map_axes.draw_gridlines()
-    if IS_DEG_MIN_FORMAT:
+    if is_deg_min_format:
         map_axes.express_in_deg_min_format()
     map_axes.narrow_down_the_plot_area()
     print("Base map is successfully made!")
