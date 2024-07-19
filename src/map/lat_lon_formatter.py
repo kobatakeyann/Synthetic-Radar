737c1a7a8cbc5613e29e0ad73ff1d4b5caea254f
kobatakeyann <suu_hppa@outlook.jp>
2024-07-19 09:35:10 +0900

    add explanation of functions

--- src/map/lat_lon_formatter.py
+++ src/map/lat_lon_formatter.py
@@ -1,4 +1,15 @@
 def format_longitude(lon: float, _) -> str:
+    """Function passed as the argument of 'set_major_formatter'.
+    The second argument must be explicitly stated for the use of the function.
+    Return a label of longitude in [degree°minute'E] format.
+
+    Args:
+        lon (float): longitude in float format.
+        _ : necessary for the use of set_major_formatter
+
+    Returns:
+        str: longitude in [degree°minute'E] format
+    """
     degrees = int(lon)
     minutes = abs(int((lon - degrees) * 60))
     if minutes == 0:
@@ -7,6 +18,17 @@ def format_longitude(lon: float, _) -> str:
 
 
 def format_latitude(lat: float, _) -> str:
+    """Function passed as the argument of 'set_major_formatter'.
+    The second argument must be explicitly stated for the use of the function.
+    Return a label of latitude in [degree°minute'E] format.
+
+    Args:
+        lat (float): latitude in float format.
+        _ : necessary for the use of set_major_formatter
+
+    Returns:
+        str: latitude in [degree°minute'E] format
+    """
     degrees = int(lat)
     minutes = abs(int((lat - degrees) * 60))
     if minutes == 0:
